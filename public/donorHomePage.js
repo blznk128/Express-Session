60320f8334b36a6c9c25a2a18a31b182be7df333
Benny Borunda <blznk128@yahoo.com>
2020-10-06 17:18:59 -0600

    made all the non profits show up on one row

--- public/donorHomePage.js
+++ public/donorHomePage.js
@@ -35,19 +35,33 @@ function goToLogOff(userId) {
   //   }
   // })
 
+  // $.get("api/allNonProfits", function(data) {
+  //   for ( let i = 0; i < data.length; i++) {
+  //     console.log(data)
+  //     nonProfitList.push(data[i].nonProfitName)
+  //     // listOfNonProfits.append("<li id = " + data[i].id + ">" + nonProfitList[i] + "<button class = 'edit'>" + "see profile" + "</button>" +"</li>")
+  //     $(".edit").append("<div class='row>'" + "<div class='col s6 m7'>" + "<div class='card small'>" + 
+  //     "<div class='card-image'>" + "<img src='https://3er1viui9wo30pkxh1v2nh4w-wpengine.netdna-ssl.com/wp-content/uploads/prod/sites/45/2019/11/MS_Penguin-Counting-Story_1900x800.jpg'>" + "<span class='card-title'>" + nonProfitList[i] + 
+  //     "</span>" + "</div>" + "<div class='card-content'>" + "<p>" + "blah blah blah blah" + "</p>" + "</div>" + "<div class='card-action'>" + 
+  //     "<a id =" + data[i].id + " href='#'" + " class='testOne'"+">" + nonProfitList[i] + "</a>" + "</div>" + "</div>" + "</div>" + "</div>")
+  //     // $(".edit").append("<a id =" + data[i].id + " href='#'" + " class='testOne'"+">" + nonProfitList[i] + "</a>" + "             ")
+  //   }
+  // })
+  
   $.get("api/allNonProfits", function(data) {
     for ( let i = 0; i < data.length; i++) {
       console.log(data)
       nonProfitList.push(data[i].nonProfitName)
-      // listOfNonProfits.append("<li id = " + data[i].id + ">" + nonProfitList[i] + "<button class = 'edit'>" + "see profile" + "</button>" +"</li>")
-      $(".edit").append("<div class='row>'" + "<div class='col s12 m7'>" + "<div class='card'>" + 
-      "<div class='card-image'>" + "<img src='https://3er1viui9wo30pkxh1v2nh4w-wpengine.netdna-ssl.com/wp-content/uploads/prod/sites/45/2019/11/MS_Penguin-Counting-Story_1900x800.jpg'>" + "<span class='card-title'>" + nonProfitList[i] + 
-      "</span>" + "</div>" + "<div class='card-content'>" + "<p>" + "blah blah blah blah" + "</p>" + "</div>" + "<div class='card-action'>" + 
-      "<a id =" + data[i].id + " href='#'" + " class='testOne'"+">" + nonProfitList[i] + "</a>" + "</div>" + "</div>" + "</div>" + "</div>")
-      // $(".edit").append("<a id =" + data[i].id + " href='#'" + " class='testOne'"+">" + nonProfitList[i] + "</a>" + "             ")
+      
+      $(".edit").append("<div class='col s3 m2'>" + "<div class='card'>" + "<div class='card-image'>" + 
+      "<img src='http://www.ilikewallpaper.net/ipad-wallpapers/download/2268/Square-Pattern-ipad-wallpaper-ilikewallpaper_com.jpg'>" + "<span class='card-title'>" + nonProfitList[i] + 
+      "</span>" + "</div>" + "<div class='card-content'>" + "<p>" + "blah " + "</p>" + "</div>" + "<div class='card-action'>" + 
+      "<a id =" + data[i].id + " href='#'" + " class='testOne'"+">" + nonProfitList[i] + "</a>" + "</div>" + "</div>" + "</div>")
+      
     }
   })
-  
+
+
 
 
 function goToNonProfit() {
