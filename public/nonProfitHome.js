ac6304aa0e5014bceb272ecb00992a43e1d4dfb6
Benny Borunda <blznk128@yahoo.com>
2020-10-13 13:55:25 -0600

    finished up

--- public/nonProfitHome.js
+++ public/nonProfitHome.js
@@ -1,6 +1,8 @@
 function getNonProfit(data) {
     $.get("/api/nonProfitSession", function(data) {
       console.log(data)
+      $("#nonProfitUser").append(data.nonProfitName)
+      $("#aboutMeSummary").append(data.aboutMe)
     })
   };
 
